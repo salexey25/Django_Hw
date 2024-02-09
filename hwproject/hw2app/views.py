45294963b62f201249b6430d85d1acf676808193
salexey25 <salexey25@gmail.com>
2024-02-09 15:02:27 +0300

    gitignore commit v1

--- hwproject/hw2app/views.py
+++ hwproject/hw2app/views.py
@@ -5,7 +5,7 @@ from django.utils.timezone import timedelta
 
 # Create your views here.
 def history_of_orders(request):
-    client_id = 2 #Здесь надо подставлять ID номер клиента из базы данных
+    client_id = 2
     orders_last_7_days = OrderModel.objects.filter(customer_id=client_id, date_order__gte=timezone.now() - timedelta(days=7))
     orders_last_30_days = OrderModel.objects.filter(customer_id=client_id, date_order__gte=timezone.now() - timedelta(days=30))
     orders_last_365_days = OrderModel.objects.filter(customer_id=client_id,date_order__gte=timezone.now() - timedelta(days=365))
